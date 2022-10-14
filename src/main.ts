9309367e26565acbecf254cb06f9d941aff4ad57
lmcamacho <lm.camacho@mavi.mx>
2022-10-14 18:06:43 -0500

    Start frontend app of Pizza Restaurant

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
+  <div class="h2 mb-0">
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+    <ui5-checkbox></ui5-checkbox>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import "@ui5/webcomponents/dist/CheckBox.js";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
