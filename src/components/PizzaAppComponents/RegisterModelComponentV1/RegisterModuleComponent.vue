2ff58a27639f7722d734b99cb972ab2515134c7d
lmcamacho <lm.camacho@mavi.mx>
2022-10-18 16:18:40 -0500

    Creation of popup and comunication with backend

--- src/components/PizzaAppComponents/RegisterModelComponentV1/RegisterModuleComponent.vue
+++ src/components/PizzaAppComponents/RegisterModelComponentV1/RegisterModuleComponent.vue
@@ -2,17 +2,17 @@
 form.row.g-3.formregister
     .col-md-6
       ui5-label.form-label(for='inputEmail4' required show-colon) Nombre
-      ui5-input#inputEmail4.form-control(type='Text' :class="{'inputError': isName}" show-clear-icon :value="txtName" @input="txtName = $event.target.value" @keyup="validateName()")
+      ui5-input#inputName.form-control(type='Text' :class="{'inputError': isName}" show-clear-icon :value="txtName" @input="txtName = $event.target.value" @keyup="validateName()")
       span(:class="{'spanError': isName}" v-if="isName") Nombre muy corto!!!
     .col-md-6
       ui5-label.form-label(for='inputEmail4' required show-colon) Apellido
-      ui5-input#inputEmail4.form-control(type='Text' :class="{'inputError': isLastname}" show-clear-icon :value="txtLastname" @input="txtLastname = $event.target.value" @keyup="validateLastname()")
+      ui5-input#inputLastname.form-control(type='Text' :class="{'inputError': isLastname}" show-clear-icon :value="txtLastname" @input="txtLastname = $event.target.value" @keyup="validateLastname()")
       span(:class="{'spanError': isLastname}" v-if="isLastname") Apellido muy corto!!!
     .col-md-6
       ui5-label.form-label(for='inputPassword4' required show-colon ) Contraseña
       ui5-input#inputPassword4.form-control(type='Password' :class="{'inputError': isPassword}" show-clear-icon :value="txtPassword" @input="txtPassword = $event.target.value" @keyup="validatePassword()")
       span(:class="{'spanError': isPassword}", v-if="isPassword") La contraseña es muy corta!!!
-    .col-12
+    .col-md-6
       ui5-label.form-label(for='inputAddress' required show-colon) Telefono
       ui5-input#inputAddress.form-control(type='Text' :class="{'inputError': isTelephone}" placeholder='1234 Main St' show-clear-icon :value="txtTelephone" @input="txtTelephone = $event.target.value" @keyup="validateTelephone()")
       span(:class="{'spanError': isTelephone}" v-if="isTelephone") Telefono ingresado incorrectamente!!!
@@ -20,11 +20,11 @@ form.row.g-3.formregister
       ui5-label.form-label(for='inputAddress2' required show-colon) Direccion
       ui5-input#inputAddress2.form-control(type='Text' :class="{'inputError': isAddress}" placeholder='Apartment, studio, or floor' show-clear-icon :value="txtAddress" @input="txtAddress = $event.target.value" @keyup="validateAddress()")
       span(:class="{'spanError': isAddress}", v-if="isAddress") Direccion muy corta o incompleta!!!
-    .col-md-4
+    .col-md-5
       ui5-label.form-label(for='inputCity' required show-colon) Ciudad
       ui5-input#inputCity.form-control(type='Text' :class="{'inputError': isCity}" show-suggestions show-clear-icon :value="txtCity" @input="txtCity = $event.target.value" @keyup="validateCity()")
       span(:class="{'spanError': isCity}" v-if="isCity") Campo ciudad incompleta!!!
-    .col-md-4
+    .col-md-3
       ui5-label.form-label(for='inputState' required show-colon) Estado
       ui5-select#inputState(:value="txtState" :class="{'inputError': isState}" @change="txtState = $event.detail.selectedOption.value" @blur="validateState()")
         ui5-option(selected) Choose...
@@ -44,7 +44,7 @@ form.row.g-3.formregister
 <script lang="ts">
 import { defineComponent } from "vue";
 import store from "@/store/index";
-// import { Notifications } from "@/tools/Utilities";
+import { Notifications } from "@/tools/Utilities";
 import "@ui5/webcomponents/dist/features/InputSuggestions.js";
 import "@ui5/webcomponents/dist/Input.js";
 import "@ui5/webcomponents/dist/Label.js";
@@ -97,35 +97,45 @@ export default defineComponent({
             this.txtState != "" &&
             this.txtZip != 0
           ) {
-            // let notification = new Notifications();
+            let notification = new Notifications();
             // store.commit("AddUserRegistered", {
             //   Email: this.txtEmail,
             //   Password: this.txtPassword,
             // });
-            // await notification.ShowSessionLoadingMessage("register");
-            let response = await fetch("https://localhost:44376/register", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json;charset=utf-8",
-              },
-              body: JSON.stringify({
-                id: 1,
-                name: this.txtName,
-                lastname: this.txtLastname,
-                password: this.txtPassword,
-                telephone: this.txtTelephone,
-                rangePermitions: 1,
-              }),
-            });
-            console.log(await response.json());
-            this.txtName = "";
-            this.txtPassword = "";
-            this.txtTelephone = "";
-            this.txtAddress = "";
-            this.txtCity = "";
-            this.txtState = "";
-            this.txtZip = 0;
-            this.isChecked = false;
+            try {
+              await Notifications.ShowSessionLoadingMessage(
+                fetch("https://localhost:44376/register", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json;charset=utf-8",
+                  },
+                  body: JSON.stringify({
+                    id: 1,
+                    name: this.txtName,
+                    lastname: this.txtLastname,
+                    password: this.txtPassword,
+                    telephone: this.txtTelephone,
+                    rangePermitions: 1,
+                  }),
+                })
+              );
+              this.txtName = "";
+              this.txtLastname = "";
+              this.txtPassword = "";
+              this.txtTelephone = "";
+              this.txtAddress = "";
+              this.txtCity = "";
+              this.txtState = "";
+              this.txtZip = 0;
+              this.isChecked = false;
+            } catch (Exception) {
+              alert(
+                "Hubo algun error en la petición, llama al area de sistemas"
+              );
+              console.error(
+                Exception + " - Error en RegisterModuleCP - line 100 to 200"
+              );
+            }
           }
         }
       }
